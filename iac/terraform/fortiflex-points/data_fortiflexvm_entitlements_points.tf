70a3d17b5fff0d4120602b3c5c2c6f2978fc0b72
John McDonough <movinalot@gmail.com>
2024-09-11 12:56:01 -0400

    updates for point consumption, provider update

--- iac/terraform/fortiflex-points/data_fortiflexvm_entitlements_points.tf
+++ iac/terraform/fortiflex-points/data_fortiflexvm_entitlements_points.tf
@@ -5,7 +5,7 @@ locals {
 data "fortiflexvm_entitlements_points" "entitlements_points" {
   config_id  = var.config_id
   account_id = var.account_id != null ? var.account_id : null
-  start_date = var.start_date != null ? var.start_date : formatdate("YYYY-MM-DD", timeadd(timestamp(), "-720h"))
+  start_date = var.start_date != null ? var.start_date : formatdate("YYYY-MM-DD", timeadd(timestamp(), "-7200h"))
   end_date   = var.end_date != null ? var.end_date : formatdate("YYYY-MM-DD", timestamp())
 }
 
